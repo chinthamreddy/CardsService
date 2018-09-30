7890eb011c0557b39aabd03b0f502992ed33321d
VenkataMounika Chinthamreddy <mounika.venkata15@gmail.com>
2018-09-30 14:00:11 +0530

    adding a sample java entity class

--- cardsservice/src/main/java/com/boarding/card/model/Card.java
+++ cardsservice/src/main/java/com/boarding/card/model/Card.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
